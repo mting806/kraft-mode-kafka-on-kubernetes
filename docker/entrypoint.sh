792f9ca0fa7a7df81ac66c571551d5d19fad1735
mting806 <mting806@msn.com>
2024-12-25 20:39:13 +0100

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 NODE_ID=${HOSTNAME:6}
 LISTENERS="PLAINTEXT://:9092,CONTROLLER://:9093"
 ADVERTISED_LISTENERS="PLAINTEXT://kafka-$NODE_ID.$SERVICE.$NAMESPACE.svc.cluster.local:9092"
-
 CONTROLLER_QUORUM_VOTERS=""
 for i in $( seq 0 $REPLICAS); do
     if [[ $i != $REPLICAS ]]; then
@@ -12,16 +10,13 @@ for i in $( seq 0 $REPLICAS); do
         CONTROLLER_QUORUM_VOTERS=${CONTROLLER_QUORUM_VOTERS::-1}
     fi
 done
-
 mkdir -p $SHARE_DIR/$NODE_ID
-
 if [[ ! -f "$SHARE_DIR/cluster_id" && "$NODE_ID" = "0" ]]; then
     CLUSTER_ID=$(kafka-storage.sh random-uuid)
     echo $CLUSTER_ID > $SHARE_DIR/cluster_id
 else
     CLUSTER_ID=$(cat $SHARE_DIR/cluster_id)
 fi
-
 sed -e "s+^node.id=.*+node.id=$NODE_ID+" \
 -e "s+^controller.quorum.voters=.*+controller.quorum.voters=$CONTROLLER_QUORUM_VOTERS+" \
 -e "s+^listeners=.*+listeners=$LISTENERS+" \
@@ -29,7 +24,5 @@ sed -e "s+^node.id=.*+node.id=$NODE_ID+" \
 -e "s+^log.dirs=.*+log.dirs=$SHARE_DIR/$NODE_ID+" \
 /opt/kafka/config/kraft/server.properties > server.properties.updated \
 && mv server.properties.updated /opt/kafka/config/kraft/server.properties
-
 kafka-storage.sh format -t $CLUSTER_ID -c /opt/kafka/config/kraft/server.properties
-
 exec kafka-server-start.sh /opt/kafka/config/kraft/server.properties
